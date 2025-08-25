0ec0d73615e8f5a26959c1a29de170f453fdd38a
DEEPAK PRINCE W <wdeepakprince@gmail.com>
2025-08-25 21:47:31 +0530

    Create list_mutable.py

---
+++ data-types/list_mutable.py
@@ -0,0 +1,15 @@
+lista=[1, "Deepak",95.75,[1,2,3]]
+
+print("The Given Data is : ", lista)
+print("Data Type is : ", type(lista))
+print("\n")
+
+print("Checking Mutability...")
+lista [1]="Prince"
+lista [3][1] = 4
+print("Mutation Completed...")
+print("\n")
+
+print("The Muted Data is : ", lista)
+print("Data Type is : ", type(lista))
+print("\n")
