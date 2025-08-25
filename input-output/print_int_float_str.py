176f8ca690365041cc61a8164219adbd9b7c84d5
DEEPAK PRINCE W <wdeepakprince@gmail.com>
2025-08-25 21:05:50 +0530

    Create print_int_float_str.py

---
+++ input-output/print_int_float_str.py
@@ -0,0 +1,25 @@
+a=25
+b=50
+c=75
+l=77.77
+m=89.91
+n=95.52
+x="Master of "
+y="Data"
+z="Science"
+print("\n")
+print("Printing Integers...")
+print("The Value of 'a' ",type(a)," is : ",a)
+print("The Value of 'b' ",type(b)," is : ",b)
+print("The Value of 'c' ",type(c)," is : ",c)
+print("\n")
+print("Printing Floats...")
+print("The Value of 'l' ",type(l)," is : ",l)
+print("The Value of 'm' ",type(m)," is : ",m)
+print("The Value of 'n' ",type(n)," is : ",n)
+print("\n")
+print("Printing Strings...")
+print("The Value of 'x' ",type(x)," is : ",x)
+print("The Value of 'y' ",type(y)," is : ",y)
+print("The Value of 'z' ",type(z)," is : ",z)
+print("\n")
