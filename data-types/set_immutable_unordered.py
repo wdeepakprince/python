030a55af699c674427514171c41a762e8704ffc4
DEEPAK PRINCE W <wdeepakprince@gmail.com>
2025-08-25 21:48:36 +0530

    Create set_immutable_unordered.py

---
+++ data-types/set_immutable_unordered.py
@@ -0,0 +1,15 @@
+seta={1, "Deepak",95.75}
+
+print("The Given Data is : ", seta)
+print("Data Type is : ", type(seta))
+print("\n")
+
+print("Checking Mutability...")
+seta [1]="Prince"
+print("Mutation Completed...")
+print("\n")
+
+print("The Muted Data is : ", seta)
+print("Data Type is : ", type(seta))
+print("\n")
+
